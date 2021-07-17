7f36d2dfa9e968025d04239baa905f522bf89d5e
GabrielBueno <54592238+GabrielNX@users.noreply.github.com>
2021-07-17 20:21:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,6 @@ import youtube_dl
 import pafy
 import discord
 from discord.ext import commands
-import ctypes
-import ctypes.util
- 
-print("ctypes - Find opus:")
-a = ctypes.util.find_library('opus')
-print(a)
- 
-print("Discord - Load Opus:")
-b = discord.opus.load_opus(a)
-print(b)
- 
-print("Discord - Is loaded:")
-c = discord.opus.is_loaded()
-print(c)
 
 intents = discord.Intents.default()
 intents.members = True
